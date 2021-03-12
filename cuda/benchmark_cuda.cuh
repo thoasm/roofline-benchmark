6e1b152550a066936b9eecbe13b1e75cb11faa3a
Thomas Grützmacher <thomas.gruetzmacher@kit.edu>
2021-03-12 16:44:58 +0100

    Change device index computation

--- cuda/benchmark_cuda.cuh
+++ cuda/benchmark_cuda.cuh
@@ -18,7 +18,8 @@
 
 template <typename T, std::int32_t outer_work_iters,
           std::int32_t inner_work_iters, std::int32_t compute_iters>
-kernel_bytes_flops_result run_benchmark_hand(std::size_t num_elems, T input, T *data_ptr) {
+kernel_bytes_flops_result run_benchmark_hand(std::size_t num_elems, T input,
+                                             T *data_ptr) {
     const dim3 block_(default_block_size);
     const dim3 grid_(ceildiv(
         num_elems, inner_work_iters * outer_work_iters * default_block_size));
@@ -31,15 +32,16 @@ kernel_bytes_flops_result run_benchmark_hand(std::size_t num_elems, T input, T *
 
     benchmark_kernel<default_block_size, outer_work_iters, inner_work_iters,
                      compute_iters, T><<<grid_, block_>>>(input, data_ptr);
-    return get_kernel_info<outer_work_iters, inner_work_iters,
-                                  compute_iters, T>(num_elems);
+    return get_kernel_info<outer_work_iters, inner_work_iters, compute_iters,
+                           T>(num_elems);
 }
 
 template <typename ArType, typename StType, std::int32_t outer_work_iters,
           std::int32_t inner_work_iters, std::int32_t compute_iters,
           std::size_t dimensionality>
-kernel_bytes_flops_result run_benchmark_accessor(std::size_t num_elems, ArType input,
-                            StType *data_ptr) {
+kernel_bytes_flops_result run_benchmark_accessor(std::size_t num_elems,
+                                                 ArType input,
+                                                 StType *data_ptr) {
     const dim3 block_(default_block_size);
     const dim3 grid_(ceildiv(
         num_elems, inner_work_iters * outer_work_iters * default_block_size));
@@ -57,6 +59,9 @@ kernel_bytes_flops_result run_benchmark_accessor(std::size_t num_elems, ArType i
     static_assert(dimensionality == 3, "Dimensionality must be 3!");
     std::array<std::size_t, dimensionality> size{
         {outer_work_iters, inner_work_iters, total_threads}};
+    std::array<std::size_t, dimensionality - 1> stride{
+        static_cast<std::size_t>(grid_.x) * block_.x * inner_work_iters,
+        static_cast<std::size_t>(block_.x)};
     /*/
     static_assert(dimensionality == 1, "Dimensionality must be 1!");
     std::array<std::size_t, dimensionality> size{{outer_work_iters *
@@ -65,13 +70,13 @@ kernel_bytes_flops_result run_benchmark_accessor(std::size_t num_elems, ArType i
     using accessor =
         gko::acc::reduced_row_major<dimensionality, ArType, StType>;
     using range = gko::acc::range<accessor>;
-    auto acc = range(size, data_ptr);
+    auto acc = range(size, data_ptr, stride);
     // Warmup
     benchmark_accessor_kernel<default_block_size, outer_work_iters,
                               inner_work_iters, compute_iters, ArType, range>
         <<<grid_, block_>>>(input, acc);
-    return get_kernel_info<outer_work_iters, inner_work_iters,
-                                  compute_iters, StType>(num_elems);
+    return get_kernel_info<outer_work_iters, inner_work_iters, compute_iters,
+                           StType>(num_elems);
 }
 
 #endif  // BENCHMARK_CUDA_CUH_
--- hip/benchmark_hip.hip.hpp
+++ hip/benchmark_hip.hip.hpp
@@ -49,19 +49,23 @@ kernel_bytes_flops_result run_benchmark_accessor(std::size_t num_elems,
     const dim3 block_(default_block_size);
     const dim3 grid_(ceildiv(
         num_elems, inner_work_iters * outer_work_iters * default_block_size));
-    auto total_threads = static_cast<std::size_t>(block_.x) * block_.y *
-                         block_.z * grid_.x * grid_.y * grid_.z;
     if (grid_.y != 1 || grid_.z != 1) {
         std::cerr << "Grid is expected to only have x-dimension!\n";
     }
     if (block_.y != 1 || block_.z != 1) {
         std::cerr << "Block is expected to only have x-dimension!\n";
     }
+    // auto total_threads = static_cast<std::size_t>(block_.x) * block_.y *
+    //                     block_.z * grid_.x * grid_.y * grid_.z;
+    auto total_threads = static_cast<std::size_t>(block_.x) * grid_.x;
 
     //*
     static_assert(dimensionality == 3, "Dimensionality must be 3!");
     std::array<std::size_t, dimensionality> size{
         {outer_work_iters, inner_work_iters, total_threads}};
+    std::array<std::size_t, dimensionality - 1> stride{
+        static_cast<std::size_t>(grid_.x) * block_.x * inner_work_iters,
+        static_cast<std::size_t>(block_.x)};
     /*/
     static_assert(dimensionality == 1, "Dimensionality must be 1!");
     std::array<std::size_t, dimensionality> size{{outer_work_iters *
@@ -70,7 +74,7 @@ kernel_bytes_flops_result run_benchmark_accessor(std::size_t num_elems,
     using accessor =
         gko::acc::reduced_row_major<dimensionality, ArType, StType>;
     using range = gko::acc::range<accessor>;
-    auto acc = range(size, data_ptr);
+    auto acc = range(size, data_ptr, stride);
     // Warmup
     hipLaunchKernelGGL(
         HIP_KERNEL_NAME(
