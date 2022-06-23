0afa36dfd2e2fe615adbebbffd4d45e9510202db
Thomas Grützmacher <thomas.gruetzmacher@kit.edu>
2022-06-23 18:14:16 +0200

    Add MI100 results with 50 repeat cycles

--- results/python/plot_roofline.py
+++ results/python/plot_roofline.py
@@ -34,7 +34,8 @@ plot_info["mi100"] = {
         "peak_fp32": 23100.0,
         "peak_fp16": 18460.0,
         "peak_bw": 1200.0,
-        "file": "../20210312_2000_MI100.csv",
+        #"file": "../20210312_2000_MI100.csv",
+        "file": "../20220623_1753_MI100.csv",
         "prefix": "mi100_",
         "filter": filt_lambda_18,
         }
